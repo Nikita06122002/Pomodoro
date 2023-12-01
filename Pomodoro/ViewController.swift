03f93b94d33695fcca3c5bf4c5d14d340413fe23
macbook <yakovenko06122002@icloud.com>
2023-12-01 18:46:16 +0300

    Init commit

---
+++ Pomodoro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by macbook on 01.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
